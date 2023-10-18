5827d18d3ced0f1a42a8320054d3020c16d3f3d1
mazitakber <bulibekovr@gmail.com>
2023-10-18 15:41:52 +0200

    second commit

--- Sales/advertisement/models.py
+++ Sales/advertisement/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(max_length=40)
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, name='rubric')
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20)
--- Sales/advertisement/views.py
+++ Sales/advertisement/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Advertisement, Rubric
 
 # Create your views here.
+
+class MyOwnListView(ListView):
+    model = Advertisement
+    context_object_name = 'adv'
+    template_name = 'advertisement.html'
+
+
+class MyOwnDetailView(DetailView):
+    model = Advertisement
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['rubric'] = Rubric.objects.all()
+
+
